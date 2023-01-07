c73fb46af0d1085d80d3759d18717cd232d6512b
tagezi <lera.goncharuk@gmail.com>
2023-01-07 12:04:24 +0300

    Added template of property table of taxa

---
+++ mli/gui/table_widget.py
@@ -0,0 +1,41 @@
+#     This code is a part of program Manual Lichen identification
+#     Copyright (C) 2022 contributors Manual Lichen identification
+#     The full list is available at the link
+#     https://github.com/tagezi/mli/blob/master/contributors.txt
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
+
+
+class TableWidget(QTableWidget):
+    def __init__(self, data, *args):
+        QTableWidget.__init__(self, *args)
+        self.data = data
+        self.setData()
+        self.resizeColumnsToContents()
+        self.resizeRowsToContents()
+
+    def setData(self):
+        horHeaders = []
+        for n, key in enumerate(sorted(self.data.keys())):
+            horHeaders.append(key)
+            for m, item in enumerate(self.data[key]):
+                newitem = QTableWidgetItem(item)
+                self.setItem(m, n, newitem)
+        self.setHorizontalHeaderLabels(horHeaders)
+
+
+if __name__ == '__main__':
+    pass
